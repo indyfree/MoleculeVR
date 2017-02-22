e798b5ffe662e7d5c96152fd93b0e03c79fdc042
Tobias Richter <trichte5@smail.uni-koeln.de>
2017-02-22 09:56:50 +0100

    include tinyply in MeshImporter

---
+++ Source/MeshGenerator/MeshImporter.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MeshGenerator.h"
+#include "MeshImporter.h"
+
+MeshImporter::MeshImporter()
+{
+	tinyply::PlyElement;
+
+}
+
+MeshImporter::~MeshImporter()
+{
+}
---
+++ Source/MeshGenerator/MeshImporter.h
@@ -0,0 +1,10 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "tinyply.h"
+
+class MESHGENERATOR_API MeshImporter
+{
+public:
+	MeshImporter();
+	~MeshImporter();
+};
