d4a7f1ddada3a032218a16f1c5852bab78af826a
Tobias Richter <trichte5@smail.uni-koeln.de>
2017-02-22 11:55:09 +0100

    generate stabilo mesh

--- Source/MeshGenerator/StabiloMesh.cpp
+++ Source/MeshGenerator/StabiloMesh.cpp
@@ -25,45 +25,55 @@ AStabiloMesh::AStabiloMesh()
 	TArray<int32> Triangles;
 
 	MeshImporter import("c:/users/tobias/mesh/stabilo.ply");
+	//TODO dont copy -> pointer or refs
 	std::vector<float> vertices =  import.GetVertices();
 	std::vector<float> normals =  import.GetNormals();
 	std::vector<uint32_t> faces =  import.GetFaces();
 	std::vector<uint8_t> colors =  import.GetColors();
 
-	// First vertex
-	Vertices.Add(FVector(0, 100, 100));
-	Normals.Add(FVector(0, 0, 1));
-	Tangents.Add(FRuntimeMeshTangent(0, -1, 0));
-	VertexColors.Add(FColor::White);
-	TextureCoordinates.Add(FVector2D(0, 0));
+	for (int i = 0; i < vertices.size() - 3; i += 3) {
+		Vertices.Add(FVector(vertices[i], vertices[i + 1], vertices[i + 2]));
+		VertexColors.Add(FColor(colors[i], colors[i+1], colors[i+2], 1));
+	}
 
-	// Second vertex
-	Vertices.Add(FVector(100, 100, 0));
-	Normals.Add(FVector(0, 0, 1));
-	Tangents.Add(FRuntimeMeshTangent(0, -1, 0));
-	VertexColors.Add(FColor::White);
-	TextureCoordinates.Add(FVector2D(0, 1));
+	for (int i = 0; i < faces.size(); ++i) {
+		Triangles.Add(faces[i]);
+	}
 
-	// Third vertex
-	Vertices.Add(FVector(100, 0, 0));
-	Normals.Add(FVector(0, 0, 1));
-	Tangents.Add(FRuntimeMeshTangent(0, -1, 0));
-	VertexColors.Add(FColor::White);
-	TextureCoordinates.Add(FVector2D(1, 1));
+	//// First vertex
+	//Vertices.Add(FVector(0, 100, 100));
+	//Normals.Add(FVector(0, 0, 1));
+	//Tangents.Add(FRuntimeMeshTangent(0, -1, 0));
+	//VertexColors.Add(FColor::White);
+	//TextureCoordinates.Add(FVector2D(0, 0));
 
-	// Fourth vertex
-	Vertices.Add(FVector(0, 0, 0));
-	Normals.Add(FVector(0, 0, 1));
-	Tangents.Add(FRuntimeMeshTangent(0, -1, 0));
-	VertexColors.Add(FColor::White);
-	TextureCoordinates.Add(FVector2D(1, 0));
+	//// Second vertex
+	//Vertices.Add(FVector(100, 100, 0));
+	//Normals.Add(FVector(0, 0, 1));
+	//Tangents.Add(FRuntimeMeshTangent(0, -1, 0));
+	//VertexColors.Add(FColor::White);
+	//TextureCoordinates.Add(FVector2D(0, 1));
 
-	Triangles.Add(0);
-	Triangles.Add(1);
-	Triangles.Add(2);
-	Triangles.Add(0);
-	Triangles.Add(2);
-	Triangles.Add(3);
+	//// Third vertex
+	//Vertices.Add(FVector(100, 0, 0));
+	//Normals.Add(FVector(0, 0, 1));
+	//Tangents.Add(FRuntimeMeshTangent(0, -1, 0));
+	//VertexColors.Add(FColor::White);
+	//TextureCoordinates.Add(FVector2D(1, 1));
+
+	//// Fourth vertex
+	//Vertices.Add(FVector(0, 0, 0));
+	//Normals.Add(FVector(0, 0, 1));
+	//Tangents.Add(FRuntimeMeshTangent(0, -1, 0));
+	//VertexColors.Add(FColor::White);
+	//TextureCoordinates.Add(FVector2D(1, 0));
+
+	//Triangles.Add(0);
+	//Triangles.Add(1);
+	//Triangles.Add(2);
+	//Triangles.Add(0);
+	//Triangles.Add(2);
+	//Triangles.Add(3);
 
 	RuntimeMesh->CreateMeshSection(0, Vertices, Triangles, Normals, TextureCoordinates, VertexColors, Tangents);
 }
