0b6b0dd4243f1180b0546f0efda61be1c1946dfc
Tobias Richter <trichte5@smail.uni-koeln.de>
2017-03-02 17:17:54 +0100

    add base class for mesh actor

---
+++ Source/MeshGenerator/MeshActorBase.cpp
@@ -0,0 +1,72 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MeshGenerator.h"
+#include "MeshActorBase.h"
+
+
+// Sets default values
+AMeshActorBase::AMeshActorBase()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	RuntimeMesh = CreateDefaultSubobject<URuntimeMeshComponent>(TEXT("Runtime Mesh"));
+	RootComponent = RuntimeMesh;
+}
+
+void AMeshActorBase::CreateMesh(const std::string & filename) 
+{
+	ImportMesh(filename);
+	SetVertexColorMaterial();
+	RuntimeMesh->CreateMeshSection(0, Vertices, Triangles, Normals, TextureCoordinates, VertexColors, Tangents);
+}
+
+// Called when the game starts or when spawned
+void AMeshActorBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMeshActorBase::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
+void AMeshActorBase::ImportMesh(const std::string & filename)
+{
+
+	MeshImporter import(filename);
+	//TODO dont copy -> pointer or refs
+	std::vector<float> vertices =  import.GetVertices();
+	std::vector<float> normals =  import.GetNormals();
+	std::vector<uint32_t> faces =  import.GetFaces();
+	std::vector<uint8_t> colors =  import.GetColors();
+
+	for (int i = 0; i < vertices.size() - 3; i += 3) {
+		Vertices.Add(FVector(vertices[i], vertices[i + 1], vertices[i + 2]));
+		VertexColors.Add(FColor(colors[i], colors[i+1], colors[i+2]));
+	}
+
+	for (int i = 0; i < faces.size(); ++i) {
+		Triangles.Add(faces[i]);
+	}
+}
+void AMeshActorBase::SetVertexColorMaterial()
+{
+
+	static ConstructorHelpers::FObjectFinder<UMaterial> Material(TEXT("Material'/Game/VirtualReality/Materials/VertexColor.VertexColor'"));
+
+	UMaterial* vertex_color_material = NULL;
+	if (Material.Object != NULL)
+	{
+		vertex_color_material = (UMaterial*)Material.Object;
+	}
+
+	// Maybe later use for dynamic materials
+	//UMaterialInstanceDynamic* dynamic_vertex_color_material = UMaterialInstanceDynamic::Create(vertex_color_material, NULL);
+
+	RuntimeMesh->SetMaterial(0, vertex_color_material);
+}
---
+++ Source/MeshGenerator/MeshActorBase.h
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "RuntimeMeshComponent.h"
+#include "MeshImporter.h"
+#include "MeshActorBase.generated.h"
+
+UCLASS()
+class MESHGENERATOR_API AMeshActorBase : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	UPROPERTY(EditAnywhere)
+	URuntimeMeshComponent* RuntimeMesh;
+	
+	// Sets default values for this actor's properties
+	AMeshActorBase();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+	// Main creation method
+	void CreateMesh(const std::string & filename);
+
+private:
+	// Import Mesh from ply file
+	void ImportMesh(const std::string & filename);
+
+	// Set Material to VertexColor to display vertex colors
+	void SetVertexColorMaterial();
+
+	TArray<FVector> Vertices;
+	TArray<FVector> Normals;
+	TArray<FRuntimeMeshTangent> Tangents;
+	TArray<FColor> VertexColors;
+	TArray<FVector2D> TextureCoordinates;
+	TArray<int32> Triangles;
+
+};
