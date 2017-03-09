75f26d9b0f2e88448b2a0d5db20e5ee5bccc0b1d
Tobias Richter <trichte5@smail.uni-koeln.de>
2017-03-09 17:00:16 +0100

    add molecule mesh

---
+++ Source/MeshGenerator/MoleculeMesh.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MeshGenerator.h"
+#include "MoleculeMesh.h"
+
+
+// Sets default values
+AMoleculeMesh::AMoleculeMesh()
+{
+	Super::CreateMesh("c:/users/tobias/mesh/4cs4.obj");
+}
+
+// Called when the game starts or when spawned
+void AMoleculeMesh::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMoleculeMesh::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
---
+++ Source/MeshGenerator/MoleculeMesh.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "MeshActorBase.h"
+#include "MoleculeMesh.generated.h"
+
+UCLASS()
+class MESHGENERATOR_API AMoleculeMesh : public AMeshActorBase
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AMoleculeMesh();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+	
+	
+};
