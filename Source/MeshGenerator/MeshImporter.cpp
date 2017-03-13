82a4d32e946fe617bca532f2795ef66ca9f1e4a3
Tobias Richter <trichte5@smail.uni-koeln.de>
2017-03-13 14:32:52 +0100

    refactor to vector.push_back

--- Source/MeshGenerator/MeshImporter.cpp
+++ Source/MeshGenerator/MeshImporter.cpp
@@ -22,44 +22,69 @@ void MeshImporter::ReadFile(const char* filename)
 		aiComponent_BONEWEIGHTS |
 		aiComponent_ANIMATIONS |
 		aiComponent_LIGHTS |
-		aiComponent_CAMERAS
+		aiComponent_CAMERAS |
+		aiComponent_TEXTURES |
+		aiComponent_TEXCOORDS
 	);
 
-	const aiScene* scene = importer.ReadFile(filename,
-		aiProcess_PreTransformVertices |
-		aiProcess_JoinIdenticalVertices |
-		aiProcess_RemoveComponent |
-		aiProcess_OptimizeGraph |
-		aiProcess_OptimizeMeshes |
-		aiProcess_SortByPType |
-		aiProcess_Triangulate);
+#define max_Quality_Preset ( \
+	aiProcess_CalcTangentSpace				|  \
+	aiProcess_GenSmoothNormals				|  \
+	aiProcess_JoinIdenticalVertices			|  \
+	aiProcess_ImproveCacheLocality			|  \
+	aiProcess_LimitBoneWeights				|  \
+	aiProcess_RemoveRedundantMaterials      |  \
+	aiProcess_SplitLargeMeshes				|  \
+	aiProcess_Triangulate					|  \
+	aiProcess_GenUVCoords                   |  \
+	aiProcess_SortByPType                   |  \
+	aiProcess_FindDegenerates               |  \
+	aiProcess_FindInvalidData               |  \
+	aiProcess_FindInstances                 |  \
+	aiProcess_ValidateDataStructure         |  \
+	aiProcess_OptimizeMeshes                |  \
+	aiProcess_OptimizeGraph					|  \
+	0 )
+
+#define molecule_Preset ( \
+	aiProcess_RemoveComponent				|  \
+	aiProcess_SplitLargeMeshes				|  \
+	aiProcess_GenSmoothNormals				|  \
+	aiProcess_Triangulate					|  \
+	aiProcess_JoinIdenticalVertices			|  \
+	aiProcess_OptimizeMeshes                |  \
+	aiProcess_OptimizeGraph					|  \
+	aiProcess_PreTransformVertices			|  \
+	0 )
+
+	// TODO revert last two presets to gather color
+	const aiScene* scene = importer.ReadFile(filename, molecule_Preset);
 
 
-	const struct aiMesh* mesh = scene->mMeshes[0];
+	for (unsigned int m = 0; m < scene->mNumMeshes; ++m) {
+		const struct aiMesh* mesh = scene->mMeshes[m];
 
-	vertices_ = vector<float>(mesh->mNumVertices);
-	for (unsigned int i = 0; i <= mesh->mNumVertices; i+=3) {
-		aiVector3D vertex = mesh->mVertices[i];
-		vertices_[i] = vertex.x;
-		vertices_[i + 1] = vertex.y;
-		vertices_[i + 2] = vertex.z;
-	}
-	
-	normals_ = vector<float>(mesh->mNumVertices);
-	for (unsigned int i = 0; i <= mesh->mNumVertices; i+=3) {
-		aiVector3D normal = mesh->mNormals[i];
-		normals_[i] = normal.x;
-		normals_[i + 1] = normal.y;
-		normals_[i + 2] = normal.z;
-	}
-
-	faces_ = vector<uint32_t>(mesh->mNumFaces * 3);
-	for (unsigned int i = 0; i < mesh->mNumFaces; ++i) {
-		aiFace face = mesh->mFaces[i];
-		int face_index = i * 3;
-		for (int j = 0; j < 3; ++j) {
-			faces_[face_index + j] = face.mIndices[j];
+		for (unsigned int i = 0; i < mesh->mNumVertices; ++i) {
+			aiVector3D vertex = mesh->mVertices[i];
+			vertices_.push_back(vertex.x);
+			vertices_.push_back(vertex.y);
+			vertices_.push_back(vertex.z);
 		}
+
+		for (unsigned int i = 0; i < mesh->mNumVertices; ++i) {
+			aiVector3D normal = mesh->mNormals[i];
+			normals_.push_back(normal.x);
+			normals_.push_back(normal.y);
+			normals_.push_back(normal.z);
+		}
+
+		for (unsigned int i = 0; i < mesh->mNumFaces; ++i) {
+			aiFace face = mesh->mFaces[i];
+			for (int j = 0; j < 3; ++j) {
+				faces_.push_back(face.mIndices[j]);
+			}
+		}
+
 	}
 }
 
